4f453a68a69efe473d2ca6c2a0890459ee922cbc
tiffboors29 <tiffanyboorsma@gmail.com>
2015-08-07 09:12:40 -0400

    Fix job-show, create and destroy functions

--- index.js
+++ index.js
@@ -104,6 +104,7 @@ $(function() {
 
   // job update click handler - for admin only
   $("#job-update").on('click', function(){
+    debugger;
     $.ajax(sa + '/jobs/' + $('#job-id').val(), {
       method: 'PATCH',
       data: {
